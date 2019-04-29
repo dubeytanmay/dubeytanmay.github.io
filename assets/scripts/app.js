68ee6fef1dc79dc71b15c66b13cfb75f10039949
dubeytanmay <50036416+dubeytanmay@users.noreply.github.com>
2019-04-29 01:14:30 -0700

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCFCw_eTJi11deH0Y-KREjuNnPNCcoOADw",
+    authDomain: "androidwatch-poland.firebaseapp.com",
+    databaseURL: "https://androidwatch-poland.firebaseio.com",
+    projectId: "androidwatch-poland",
+    storageBucket: "androidwatch-poland.appspot.com",
+    messagingSenderId: "396410198351"
+};
+firebase.initializeApp(config);
